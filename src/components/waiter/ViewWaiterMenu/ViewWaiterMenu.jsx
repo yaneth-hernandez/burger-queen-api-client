3f8e4190696c9001a9d70f31a92bd1bbde094899
yaneth-hernandez <yahecaceres@gmail.com>
2023-01-16 21:37:28 -0500

    menú y pedido responsivo horizontal

--- src/components/waiter/ViewWaiterMenu/ViewWaiterMenu.jsx
+++ src/components/waiter/ViewWaiterMenu/ViewWaiterMenu.jsx
@@ -4,40 +4,41 @@ import { Menu } from '../Menu/Menu'
 import { ShoppingCart } from '../ShoppingCart/ShoppingCart'
 import { requestGetProducts } from '../../../helpers/API_request/productRequest'
 import { CartProvider } from '../CartContext/CartContext'
+import '../ShoppingCart/ShoppingCart.scss'
+
+export const ViewWaiterMenu = () => {
+    const token = localStorage.getItem('Token')
+    const [menu, setMenu] = useState([])
+    const [typeMenu, setTypeMenu] = useState('Desayuno')
 
 
-export const ViewWaiterMenu = ()=>{
-const token = localStorage.getItem('Token')
-const [menu, setMenu] = useState([]) 
-const [typeMenu, setTypeMenu ] = useState('Desayuno')
+    useEffect(() => {
+        requestGetProducts(token)
+            .then(res => res.json())
+            .then((res) => {
+                setMenu(res.filter(element => {
+                    if (element.type === typeMenu) {
 
-
-useEffect(()=>{
-    requestGetProducts(token)
-    .then(res => res.json())
-    .then((res)=>{
-        setMenu(res.filter(element => {
-            if(element.type === typeMenu){
-               
-                return {
-                    name: element.name,
-                    id: element.id,
-                    price: element.price,
-                    image: element.image,
-                    type: element.type,
-                  }
-            }
-        }));
-    })
-},[typeMenu])
-    return(
-      <CartProvider>
-        <>
-            <HeaderWaiterMenu setTypeMenu={setTypeMenu} typeMenu={typeMenu}/>
-            <Menu menu={menu} typeMenu={typeMenu}/>
-            <ShoppingCart typeMenu={typeMenu}/>
-            
-        </>
+                        return {
+                            name: element.name,
+                            id: element.id,
+                            price: element.price,
+                            image: element.image,
+                            type: element.type,
+                        }
+                    }
+                }));
+            })
+    }, [typeMenu])
+    return (
+        <CartProvider>
+            <>
+                <HeaderWaiterMenu setTypeMenu={setTypeMenu} typeMenu={typeMenu} />
+                <div className='orderContainer'>
+                    <Menu menu={menu} typeMenu={typeMenu} />
+                    <ShoppingCart typeMenu={typeMenu} />
+                </div>
+            </>
         </CartProvider>
     )
 }
